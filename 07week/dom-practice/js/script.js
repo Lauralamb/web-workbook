9b4d7f186df42624004b9c6941814770324e2c44
Laura Lamb <jklm1304@gmail.com>
2018-11-28 16:37:04 -0600

    shopping cart

--- 07week/dom-practice/js/script.js
+++ 07week/dom-practice/js/script.js
@@ -1,73 +1,60 @@
 'use strict';
 
 window.onload = function() {
+  //h2 header count - working
   let count = document.getElementsByTagName ('ul');
-  // console.log(count[0].children.length);
-
   let newh2 = document.createElement('h2');
   newh2.innerHTML = "You have " + count[0].children.length +  " items in your shopping cart";
   document.getElementsByTagName('h1')[0].after(newh2);  
 
-  // let input = document.createElement ('input');
-  // input.type = "text";
-  // input.innerHTML = "type";
-  // document.getElementsByTagName('ul')[0].after('input');
-
+  // create text input box for list
   createNewAddField();
-
-//text box
-function createNewAddField(){
-  let newInput = document.createElement('input');
-  newInput.setAttribute("id",'item-text');
-  document.getElementsByTagName('ul')[0].after(newInput);
-  let inputField = document.getElementById('item-text');
-  inputField.setAttribute("placeholder", "Type to add item.");
-
-  //add item button
-let newButton = document.createElement('button');
-newButton.setAttribute("id",'add-button');
-document.getElementById('item-text').after(newButton);
-newButton.innerHTML = "Add item"; //text on the button
-newButton.onclick = addItemsExistingList;
-
-//adds remove button
-let removeButton = document.createElement('button');
-removeButton.setAttribute("id",'remove-button');
-document.getElementById('add-button').after(removeButton);
-removeButton.innerHTML = "Remove item"; //text on the button
-removeButton.onclick = removeItemsExistingList;
-
-//I don't think i need this -  document.getElementById("add-item").appendChild(newInput);
-// document.getElementById("add-item").appendChild(newButton);
-}
-// this adds new items to the list and works correctly
-function addItemsExistingList(event){
-  let userInput= document.getElementById('item-text').value;
-  let listNode = document.getElementById('items');
-  let liNode = document.createElement("LI");
-  let  txtNode = document.createTextNode(userInput);
-liNode.appendChild(txtNode);
-listNode.appendChild(liNode);
-document.getElementById('item-text').value=''; //resets input box to remove previous input
-newh2.innerHTML = "You have " + count[0].children.length +  " items in your shopping cart"; //updates the count
-}
-//attempt to remove
-// function removeItemsExistingList(event) {
-  let items = document.querySelectorAll("#items li"),
-  tab = [], liIndex;
-  for (var i = 0; i < items.length; i++){
-    tab.push(items[i].innerHTML);
+  function createNewAddField(){
+    let newInput = document.createElement('input');
+    newInput.setAttribute("id",'item-text');
+    document.getElementsByTagName('ul')[0].after(newInput);
+    let inputField = document.getElementById('item-text');
+    inputField.setAttribute("placeholder", "Type to add item.");
+  
+  //create button to add for list
+  let newButton = document.createElement('button');
+  newButton.setAttribute("id",'add-button');
+  document.getElementById('item-text').after(newButton);
+  newButton.innerHTML = "Add item"; //text on the button
+  newButton.onclick = addItemsExistingList;
   }
-  for (var i = 0; i < items.length; i++){
-    items[i].onclick = function(){
-    liIndex = tab.indexOf(this.innerHTML);
-    console.log(this.innerHTML + "INDEX =" + liIndex);
-    };
+  //create new li for list
+  //makes add button functional with count working
+  function addItemsExistingList(event){
+    let userInput= document.getElementById('item-text').value;
+    let listNode = document.getElementById('items');
+    let liNode = document.createElement("LI");
+    let  txtNode = document.createTextNode(userInput);
+  liNode.appendChild(txtNode);
+  listNode.appendChild(liNode);
+  //resets input box to remove previous input
+  document.getElementById('item-text').value=''; 
+  newh2.innerHTML = "You have " + count[0].children.length +  " items in your shopping cart"; //updates the count
   }
-  function removeItemsExistingList(event) {
-    items[liIndex].parentNode.removeChild(items[liIndex]);
-    newh2.innerHTML = "You have " + count[0].children.length +  " items in your shopping cart"; //updates the count
+  
+  //create button to remove on each li
+  let removeButton = document.createElement('button');
+  removeButton.setAttribute("id",'remove-button');
+  document.getElementById('add-button').after(removeButton);
+  removeButton.innerHTML = "Remove item"; //text on the button
+  removeButton.onclick = removeItemsExistingList;
+  
+  //make remove button work - you must click the button and then click the item to remove it.
+  function removeItemsExistingList (){
+    var listItems = document.getElementsByTagName("li"); 
+  for (var i = 0; i < listItems.length; i++) {
+    listItems[i].onclick = function() {this.parentNode.removeChild(this);
+      newh2.innerHTML = "You have " + count[0].children.length +  " items in your shopping cart"; //updates the count
+    }
   }
-
-
 }
+}
+
+//I did not realize that you wanted a button on each line - I must have missed that on the day I did remote class.
+
+
